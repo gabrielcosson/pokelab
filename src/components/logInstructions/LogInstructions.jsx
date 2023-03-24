fcb5cfe69159770b7758f6c8e67ff281c806ce20
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-03-24 14:32:16 -0500

    Bringing Pokemos from DB and showing them in Home

--- src/components/logInstructions/LogInstructions.jsx
+++ src/components/logInstructions/LogInstructions.jsx
@@ -1,12 +1,13 @@
 import LogInstructionsStyle from './LogInstructions.module.css';
 import image from '../../assets/pokeball.png';
+import { Link } from 'react-router-dom';
 
 
 const LogInstructions = (props) => {
     return(
         <div className={LogInstructionsStyle.instructionsContainer}>
             <img className={LogInstructionsStyle.image} src={image} alt= 'pokeball'/>
-            <h3 className={LogInstructionsStyle.instructions}>Please enter your credentials to log in. If you do not have an account, please use the <span>Sign Up</span> button</h3>
+            <h3 className={LogInstructionsStyle.instructions}>Please enter your credentials to log in. If you do not have an account, please use the <span><Link to = "/home">Sign Up</Link></span> button</h3>
         </div>
     );
 };
--- src/components/pokemonCard/PokemonCard.jsx
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -1,10 +1,11 @@
 import TypeCard from '../typeCard/TypeCard.jsx';
 import PokemonCardStyle from './PokemonCard.module.css';
+import Types from '../../../types.json';
 
 const PokemonCard = (props) => {
     return(
-        <div className={PokemonCardStyle.container} style={{background: "linear-gradient(to left,"+props.primaryColor+", "+props.secondaryColor+")"}}>
-            <h1 className={PokemonCardStyle.name}>{props.name}</h1>
+        <div className={PokemonCardStyle.container} style={{background: "linear-gradient(to right,"+Types[props.mainType].primaryColor+", "+Types[props.mainType].secondaryColor+")"}}>
+            <h1 className={PokemonCardStyle.name}>{props.name.charAt(0).toUpperCase()+props.name.substr(1,12).toLowerCase()}</h1>
             <img className = {PokemonCardStyle.image} src={props.image}/>
             <div className={PokemonCardStyle.typeContainer}>
             {(props.types).map(type => <TypeCard type={type}></TypeCard>)}
