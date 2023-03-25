7006b264a5d87bf8e0902ec40e9869d9851dfa70
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-25 17:34:13 -0500

    updating details of pokemon

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,12 @@
 import { Component } from 'react';
 import DetailsStructure from '../components/detailsStructure/DetailsStructure';
 
-class Details extends Component{
-    render(){
+const Details = ()=>{
+    
         return(
             <DetailsStructure></DetailsStructure>           
         )
-    }
+    
 }
 
 export default Details;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     resultes: [],
   });
   const { data, isLoadin, hasError } = useFetchGet(
-    "http://localhost:8080/pokedex/pokemon?quantity=12&offset=70"
+    "http://localhost:8080/pokedex/pokemon?quantity=12&offset=90"
   );
   if (!isLoadin) {
     console.log(data);
