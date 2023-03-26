c046f7a09ed5c1c4c3c9732d1cebe975a2f10a54
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-26 17:50:36 -0500

    adding info evolutions

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,14 +2,24 @@ import { Component } from 'react';
 import DetailsStructure from '../components/detailsStructure/DetailsStructure';
 import useFetchGet from "../hooks/useFetchGet";
 import PropTypes from 'prop-types';
-const Details = ({pokemonName})=>{
-
-    console.log(`esto es una prueba name ${pokemonName}`)
+const Details = ({pokemonName,language})=>{
     const { data, isLoadin, hasError } = useFetchGet(
-      `http://localhost:8080/pokedex/de/pokemon?value=${pokemonName}`
+      `http://localhost:8080/pokedex/${language}/pokemon?value=${pokemonName}`
     );
 
-    console.log('Esto trayendo info ' , data );
+     const {
+       data: dataEvolution,
+       isLoadin: isLoadinEvolution,
+       hasError: hasErrorEvolution,
+     } = useFetchGet(
+       `http://localhost:8080/pokedex/${language}/pokemon/evolution-chain?name=${pokemonName}`
+     );
+     
+    if (isLoadin === false && isLoadinEvolution == false) {
+      console.log("Esto trayendo info ", data);
+      console.log("Esto trayendo evolution ", dataEvolution);
+    } 
+    
         return (
           <>
             <h1>Hola</h1>
@@ -20,10 +30,12 @@ const Details = ({pokemonName})=>{
     
 }
 Details.propTypes = {
-    pokemonName: PropTypes.string.isRequired
+    pokemonName: PropTypes.string.isRequired,
+    language: PropTypes.string.isRequired
 }
 
 Details.defaultProps = {
-    pokemonName: 'pikachu'
+    pokemonName: 'pikachu',
+    language: 'en'
 }
 export default Details;
