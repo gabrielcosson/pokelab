b7fc2872f7b585e57a5388cf9cee62993930589b
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-03-27 13:50:16 -0500

    Changing some style from Details Page

---
+++ src/components/homeStructure/HomeStructure.jsx
@@ -0,0 +1,16 @@
+import HomeStructureStyle from './HomeStructure.module.css';
+import OutHeader from '../outHeader/OutHeader';
+import LogInstructions from '../logInstructions/LogInstructions';
+import {LogInContainer} from '../logInContainer/LogInContainer';
+
+
+const HomeStructure = (props) => {
+    return(
+        <>
+        <div className={HomeStructureStyle.page}></div>
+        
+        </>
+    );
+};
+
+export default HomeStructure;
