a2e185250b87ebb552cdbc2222114a7b4e626448
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-27 21:17:40 -0500

    adding more info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,25 @@ import PokemonList from "../components/pokemonList/PokemonList";
 import useFetchGet from "../hooks/useFetchGet";
 import Spinner from "../components/spinner/Spinner";
 
-const Home = () => {
+const Home = ({ type = "fire" }) => {
   const { getFetch } = useFetchGet();
   const [pokemons, setPokemons] = useState("");
   const [url, setUrl] = useState({
     quantity: 0,
     resultes: [],
   });
- 
+
   const { data, isLoadin, hasError } = useFetchGet(
     "http://localhost:8080/pokedex/pokemon?quantity=12&offset=0"
   );
+
+  const {
+    data: dataType,
+    isLoadin: isLoadinType,
+    hasError: hasErrorType,
+  } = useFetchGet(`http://localhost:8080/pokedex/type?pokemonType=${type}`);
+
+  console.log('data Type ' , {dataType});
 
   return (
     <>
