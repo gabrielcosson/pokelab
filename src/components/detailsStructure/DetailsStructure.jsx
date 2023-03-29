3f2a9154f981b6674f4c87f249dbb41a4fc73d0a
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-03-28 21:44:25 -0500

    Back button from Details

--- src/components/detailsStructure/DetailsStructure.jsx
+++ src/components/detailsStructure/DetailsStructure.jsx
@@ -4,19 +4,22 @@ import { BurgerMenuContext } from '../context/burgerMenuContext';
 import DetailsContainer from '../detailsContainer/DetailsContainer';
 import LanguageOptions from '../languageOptions/LanguageOptions';
 import DetailsStructureStyle from './DetailsStructure.module.css';
+import backButton from '../../assets/backButton.png'
+import { Link } from 'react-router-dom';
 
 
 const DetailsStructure = (props) => {
+    console.log(props.data)
     const { widthBurgerMenu, widthList } = useContext(BurgerMenuContext);
     return(
         <>
-            
             <div className={DetailsStructureStyle.allContainer}>
                 <div className={DetailsStructureStyle.burgerMenu} style={widthBurgerMenu}>
                     <BurgerMenu></BurgerMenu>
                 </div>
-                    <div className={DetailsStructureStyle.details} style={widthList}>
-                        <div className={DetailsStructureStyle.detailsHeader}>
+                <div className={DetailsStructureStyle.details} style={widthList}>
+                    <div className={DetailsStructureStyle.detailsHeader}>
+                    <Link to= {`/home/${props.data.language}`}><img className={DetailsStructureStyle.back} src={backButton}></img></Link>
                         <div className={DetailsStructureStyle.titleContainer}>
                             <h1 className={DetailsStructureStyle.title}>Pokedex</h1>
                         </div>
