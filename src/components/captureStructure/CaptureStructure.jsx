a335f8acfbd7ce9b52815f12f9ad06f9cdd0dbd1
Juan Andrés Pico <61892156+Jandres1420@users.noreply.github.com>
2023-03-30 13:40:19 -0500

    seatch bar pokemon

--- src/components/captureStructure/CaptureStructure.jsx
+++ src/components/captureStructure/CaptureStructure.jsx
@@ -8,12 +8,13 @@ import { useParams } from "react-router-dom";
 import InHeader from "../inHeader/InHeader";
 import { AppContext } from "../appContext/AppContext";
 import useFetchGetHeaders from "../../hooks/useFetchGetHeaders";
+import SearchLanguageCapture from "../searchLanguageCaptures/SearchLanguageCapture";
 
 const CaptureStructure = (props) => {
   const { language } = useParams();
   const [pageOffset, setPageOffset] = useState(0);
   const { widthBurgerMenu, widthList, globalUser } = useContext(AppContext);
-
+  console.log("lenguaje", language);
   const { data, isLoadin, hasError } = useFetchGetHeaders(
     `http://localhost:8080/pokedex/pokemon-trainer/${globalUser.username}/pokemon?quantity=12&offset=${pageOffset}&language=${language}`);
 
@@ -21,34 +22,40 @@ const CaptureStructure = (props) => {
     <>
       <InHeader username={globalUser.username}></InHeader>
       <div className={CaptureStructureStyle.complete}>
-        <div className={CaptureStructureStyle.burgerMenu} style={widthBurgerMenu}>
-          <BurgerMenu language = {language} userName={globalUser.name} userRole={globalUser.role}></BurgerMenu>
+        <div
+          className={CaptureStructureStyle.burgerMenu}
+          style={widthBurgerMenu}
+        >
+          <BurgerMenu
+            language={language}
+            userName={globalUser.name}
+            userRole={globalUser.role}
+          ></BurgerMenu>
         </div>
         <div className={CaptureStructureStyle.pokemonList} style={widthList}>
-          <SearchLanguage language= {language}></SearchLanguage>
+          <SearchLanguageCapture language={language}></SearchLanguageCapture>
           <div className={CaptureStructureStyle.listHeader}>
             <div className={CaptureStructureStyle.titleContainer}>
               <h1 className={CaptureStructureStyle.title}>My Pokemons</h1>
             </div>
-            {isLoadin === false && 
-
-            <div className={CaptureStructureStyle.paginationContainer}>
-              <button
-                className={CaptureStructureStyle.button}
-                onClick={() => setPageOffset(pageOffset - 12)}
-                disabled={pageOffset == 0 ? true : false}
-              >
-                Previous
-              </button>
-              <button
-                className={CaptureStructureStyle.button}
-                onClick={() => setPageOffset(pageOffset + 12)}
-                disabled={data.results.length<12 ? true : false}
-              >
-                Next
-              </button>
-            </div>
-            }
+            {isLoadin === false && (
+              <div className={CaptureStructureStyle.paginationContainer}>
+                <button
+                  className={CaptureStructureStyle.button}
+                  onClick={() => setPageOffset(pageOffset - 12)}
+                  disabled={pageOffset == 0 ? true : false}
+                >
+                  Previous
+                </button>
+                <button
+                  className={CaptureStructureStyle.button}
+                  onClick={() => setPageOffset(pageOffset + 12)}
+                  disabled={data.results.length < 12 ? true : false}
+                >
+                  Next
+                </button>
+              </div>
+            )}
           </div>
           {isLoadin === true && <Spinner></Spinner>}
           {isLoadin === false && <PokemonList data={data}></PokemonList>}
